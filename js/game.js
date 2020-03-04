6382c030486cda3eef09f7960f0ded45c0857e3c
Fábio Costa <fabiodanielmc@gmail.com>
2020-03-04 18:12:15 +0100

    render more than 1 obstacle and losing screen with a try again button working

--- js/game.js
+++ js/game.js
@@ -4,11 +4,13 @@ class Game{
         this.player = new Player;
         this.currentPoints = 0;
         this.increment = this.increment.bind(this);
-        // this.start = this.start.bind(this);
+        this.start = this.start.bind(this);
         this.pointsId = 0;
         this.gravity = 0.4;
-        this.obstacle = new Obstacle;
+        this.obstacle = [new Obstacle];
         this.newInt = 0;
+        this.obsInt = 0;
+        this.obstDelInt = 0;
     }
 
 
@@ -25,41 +27,62 @@ class Game{
         this.newInt = setInterval(() => {
             renderEverything();
         }, 100);
+
+        this.obsInt = setInterval(()=> {
+            this.obstacle.push(new Obstacle);
+        }, 2000)
+
+        this.obstDelInt = setInterval(()=> {
+            if( this.obstacle.length > 2){
+                let randomObstacle = Math.floor(Math.random() * this.obstacle.length);
+                this.obstacle.splice(0, 1);
+            }
+        }, 3000)
     }
 
     isCollide($player, $obstacle) {
-            const $playerRect = $player.getBoundingClientRect();
-            const $obstacleRect = $obstacle.getBoundingClientRect();
-            return !(
-                $playerRect.top + $playerRect.height < $obstacleRect.top ||
-                $playerRect.top > $obstacleRect.top + $obstacleRect.height ||
-                $playerRect.left + $playerRect.width < $obstacleRect.left ||
-                $playerRect.left > $obstacleRect.left + $obstacleRect.width
-            );
+        const $playerRect = $player.getBoundingClientRect();
+        const $obstacleRect = $obstacle.getBoundingClientRect();
+        return !(
+            $playerRect.top + $playerRect.height < $obstacleRect.top ||
+            $playerRect.top > $obstacleRect.top + $obstacleRect.height ||
+            $playerRect.left + $playerRect.width < $obstacleRect.left ||
+            $playerRect.left > $obstacleRect.left + $obstacleRect.width
+        );
     }
 
     
     gameOver(){
-        let self = this;
-        debugger
         let $gameOver = document.createElement("h1");
         let $points = document.createElement("h2");
         let $restartButton = document.createElement("button");
+        let $popDiv = document.createElement("div");
         $restartButton.setAttribute("id", "restart-btn");
         $restartButton.innerHTML = "Try Again";
         $points.innerHTML = `Your score: ${this.currentPoints} barks`
-        $gameOver.innerHTML = "Ohh , You suck !";
+        $gameOver.innerHTML = "Oh, poop!";
+        $popDiv.style.width = `100%`;
+        $popDiv.style.height = `100%`;
+        $popDiv.setAttribute("id", "popup");
+
+        $body.appendChild($popDiv);
+        
         $body.appendChild($gameOver);
         $body.appendChild($points);
         $body.appendChild($restartButton);
         clearInterval(this.pointsId);
         clearInterval(this.newInt);
+        clearInterval(this.obsInt);
+        clearInterval(this.obstDelInt);
         let $restartBtn = document.querySelector("#restart-btn");
-        $restartBtn.addEventListener("click", function(){
-            self.start();
+        $restartBtn.addEventListener("click", ()=>{
+            $body.removeChild($gameOver);
+            $body.removeChild($points);
+            $body.removeChild($restartButton);
+            $body.removeChild($popDiv);
+            this.start();
             document.querySelector("#woof-sound").play();
         });
-        
     }
 }
 
--- js/render.js
+++ js/render.js
@@ -35,30 +35,48 @@ function renderPlayer() {
     
 }
 
-function obstaclePosition(){
+function obstaclePosition(i){
     let positionArr = [];
-    let obstaclePositionY = (($bodyHeight*obstacle.position[1]/10) - obstacle.size);
+    let obstaclePositionY = (($bodyHeight*obstacle[i].position[1]/10) - obstacle[i].size);
     positionArr.unshift(obstaclePositionY);
-    let obstaclePositionX = (($bodyWidth*obstacle.position[0]/10) + obstacle.size);
+    let obstaclePositionX = (($bodyWidth*obstacle[i].position[0]/10) + obstacle[i].size);
     positionArr.unshift(obstaclePositionX);
     if (positionArr[0] > - $bodyWidth){
-        obstacle.move();
+        obstacle[i].move();
     }
     return positionArr;
 }
 
 
 function renderObstacle(){
-    
-    let $obstacleImg = document.createElement("img");
-    $obstacleImg.setAttribute("src", "./images/cat_sleep_nb.gif");~
-    $obstacleImg.setAttribute("id", "obstacle");
-    $obstacleImg.style.position = `relative`;
-    $obstacleImg.style.height = `${obstacle.size}px`;
-    $obstacleImg.style.width = `${obstacle.size - 20}px`;
-    $obstacleImg.style.top = `${obstaclePosition()[1]}px`
-    $obstacleImg.style.left = `${obstaclePosition()[0]}px`;
-    $gameboard.appendChild($obstacleImg);
+
+    for( let i = 0; i < obstacle.length; i++){
+        // if($body.offsetWidth <= 1000){
+            let $obstacleImg = document.createElement("img");
+            $obstacleImg.setAttribute("src", "./images/cat_sleep_nb.gif");~
+            $obstacleImg.setAttribute("id", "obstacle");
+            $obstacleImg.style.position = `relative`;
+            $obstacleImg.style.height = `${obstacle[i].size}px`;
+            $obstacleImg.style.width = `${obstacle[i].size - 20}px`;
+            $obstacleImg.style.top = `${obstaclePosition(i)[1]}px`
+            $obstacleImg.style.left = `${obstaclePosition(i)[0]}px`;
+            $gameboard.appendChild($obstacleImg);
+        // }else if ($body.offsetWidth > 1000){
+        //     debugger
+        //     if((obstaclePosition(i)[0] < (1000 + $gameboard.offsetLeft))&&(obstaclePosition(i)[0] < $gameboard.offsetLeft)){
+        //         let $obstacleImg = document.createElement("img");
+        //         $obstacleImg.setAttribute("src", "./images/cat_sleep_nb.gif");~
+        //         $obstacleImg.setAttribute("id", "obstacle");
+        //         $obstacleImg.style.position = `relative`;
+        //         $obstacleImg.style.height = `${obstacle[i].size}px`;
+        //         $obstacleImg.style.width = `${obstacle[i].size - 20}px`;
+        //         $obstacleImg.style.top = `${obstaclePosition(i)[1]}px`
+        //         $obstacleImg.style.left = `${obstaclePosition(i)[0]}px`;
+        //         $gameboard.appendChild($obstacleImg);
+        //     }
+        // }
+        
+    }
 }
 
 
@@ -67,7 +85,8 @@ function clearScreen(){
 }
 
 function renderBackground(){
-    $gameboard.style.backgroundImage = "url('./images/background2.gif')";
+    $gameboard.style.backgroundImage = "url('./images/background3.gif')";
+    // $gameboard.style.maxWidth = `1000px` ;
 }
 
 function renderEverything(){
@@ -76,11 +95,15 @@ function renderEverything(){
     renderPlayer();
     renderObstacle();
     gravityCheck();
+
     let $player = document.querySelector("#player");
-    let $obstacle = document.querySelector("#obstacle");
-    if (game.isCollide($player, $obstacle)){
-        game.gameOver();
+    let $obstacle = document.querySelectorAll("#obstacle");
+    for (let i = 0; i < $obstacle.length; i++){
+        if (game.isCollide($player, $obstacle[i])){
+            game.gameOver();
+        }
     }
+    
 }
 
 
